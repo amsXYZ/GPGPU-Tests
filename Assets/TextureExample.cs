ff6c011433f50b8e093cf1647136d2f3474178f2
AMS <amserik@hotmail.com>
2015-09-27 18:06:23 +0200

    Warps and threads

---
+++ Assets/TextureExample.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureExample : MonoBehaviour
+{
+	public ComputeShader shader, shaderCopy;
+	
+	RenderTexture tex, texCopy;
+	
+	void Start ()
+	{
+		
+		tex = new RenderTexture(64, 64, 0);
+		tex.enableRandomWrite = true;
+		tex.Create();
+		
+		texCopy = new RenderTexture(64, 64, 0);
+		texCopy.enableRandomWrite = true;
+		texCopy.Create();
+		
+		shader.SetTexture(0, "tex", tex);
+		shader.Dispatch(0, tex.width/8, tex.height/8, 1);
+		
+		shaderCopy.SetTexture(0, "tex", tex);
+		shaderCopy.SetTexture(0, "texCopy", texCopy);
+		shaderCopy.Dispatch(0, texCopy.width/8, texCopy.height/8, 1);
+	}
+	
+	void OnGUI()
+	{
+		int w = Screen.width/2;
+		int h = Screen.height/2;
+		int s = 512;
+		
+		GUI.DrawTexture(new Rect(w-s/2,h-s/2,s,s), texCopy);
+	}
+	
+	void OnDestroy()
+	{
+		tex.Release();
+		texCopy.Release();
+	}
+}
